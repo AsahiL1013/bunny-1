ad307d6524e202509557b6f2f58908f093268e59
asahi <873184763@qq.com>
2021-12-15 17:17:52 +0800

    third commit

--- Test-Git/src/main/java/com/lx/git/Demo1.java
+++ Test-Git/src/main/java/com/lx/git/Demo1.java
@@ -4,5 +4,6 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello Git");
+        System.out.println("hello Git ~");
     }
 }
